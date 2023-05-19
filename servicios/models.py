8f570ec5f1fed0760ae3b059e5784aeca83b4bfd
Mati <matigomez2173@gmail.com>
2023-05-18 22:33:07 -0300

    Servicio

---
+++ servicios/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Servicio(models.Model):
+    nombre = models.TextField(max_length=50)
+    descripcion = models.TextField(max_length=200)
+    precio = models.IntegerField()
+    activo = models.BooleanField(default=True)
+
+
+def __str__(self):
+    return f'{self.nombre}'
