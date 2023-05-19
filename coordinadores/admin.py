310e19d07f866e4be4d9bb6b9f627e1bbb4b4172
tomaslopezz <tomaslopezm12@gmail.com>
2023-05-19 16:00:09 -0300

    commit de prueba

--- coordinadores/admin.py
+++ coordinadores/admin.py
@@ -5,7 +5,7 @@ from .models import Coordinador
 # Register your models here.
 class CoordinadorAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'apellido', 'fecha_alta', 'activo')
-    search_fields = ('nombre', 'apellido')
+    search_fields = ('nombre',)
     list_filter = ('activo',)
 
 
