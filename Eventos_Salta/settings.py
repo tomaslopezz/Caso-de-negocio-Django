8e77375182d2fe408232f45fb4b1052b6a864fa8
Mati <matigomez2173@gmail.com>
2023-05-18 22:44:29 -0300

    Nuevas aplicaciones

--- Eventos_Salta/settings.py
+++ Eventos_Salta/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reservas'
+    'reservas',
+    'servicios',
+    'empleados',
+    'coordinadores',
+    'clientes'
 ]
 
 MIDDLEWARE = [
