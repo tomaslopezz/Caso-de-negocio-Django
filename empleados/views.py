64b7ed41fbea48603862b0bf2f17df4ad0b89539
tomaslopezz <tomaslopezm12@gmail.com>
2023-05-19 21:34:53 -0300

    Se agrego la vista y url para activar_empleado

--- empleados/views.py
+++ empleados/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Empleado
 
 # Create your views here.
+def listar_empleados(request):
+    empleados = Empleado.objects.all()
+    context = {'empleados': empleados}
+
+    return render(request, 'lista_empleados.jinja', context)
+
+
+def activar_empleado(request, id):
+    empleado = Empleado.objects.get(id=id)
+    empleado.save()
+    return HttpResponse("<h1>Empleado activado con exito</h1>")
