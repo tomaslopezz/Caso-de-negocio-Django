70ff7465ce98f8b4ab23b5023631723918f10968
arielfherrera <arielfherrera@gmail.com>
2023-05-21 17:02:46 -0300

    Modelo y Registro admin de Reservas

--- reservas/models.py
+++ reservas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from clientes.models import Cliente
+from servicios.models import Servicio
+from empleados.models import Empleado
+from coordinadores.models import Coordinador
 
 # Create your models here.
+class Reserva(models.Model):
+    fecha_reserva = models.DateField()
+    fecha_servicio = models.DateField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    coordinador = models.ForeignKey(Coordinador, on_delete=models.CASCADE)
+    activo = models.BooleanField(default=True)
