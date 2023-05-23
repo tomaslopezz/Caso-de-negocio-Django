1a5f99cb4d3b7456e28471fdc6214f9758998fac
tomaslopezz <tomaslopezm12@gmail.com>
2023-05-23 16:42:22 -0300

    agregada la view para lista de coordinadores

--- coordinadores/urls.py
+++ coordinadores/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('listado', views.listar_coordinadores, name='listar-coordinadores'),
 ]
--- coordinadores/views.py
+++ coordinadores/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Coordinador
 
 # Create your views here.
+def listar_coordinadores(request):
+    coordinadores = Coordinador.objects.all()
+    context = {'coordinadores':coordinadores}
+
+    return render(request,'listado', context)
+
