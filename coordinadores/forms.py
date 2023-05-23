0ca85470927d47d6fe516896a2c2e239e9282e5e
Mati <matigomez2173@gmail.com>
2023-05-23 15:04:33 -0300

    Arreglando formulario

--- coordinadores/forms.py
+++ coordinadores/forms.py
@@ -3,12 +3,10 @@ from .models import Coordinador
 
 
 class CoordinadorForm(forms.ModelForm):
-
     class Meta:
         model = Coordinador
-        fields = {'nombre', 'apellido', 'dni', 'fecha_alta'}
+        fields = {'nombre', 'apellido', 'dni'}
         labels = {'nombre': 'Nombre',
                   'apellido': 'Apellido',
-                  'dni': 'DNI',
-                  'fecha_alta': 'Fecha de alta'
+                  'dni': 'DNI'
                   }
