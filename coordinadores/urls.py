08d83ec6fcfb8e52f0ecfba7e8208cca8647c14d
Mati <matigomez2173@gmail.com>
2023-05-23 15:05:45 -0300

    Para actualizar coordinadores

--- coordinadores/urls.py
+++ coordinadores/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('modificar/<int:id>', views.actualizar_coordinador, name='modificar-coordinador')
 ]
--- coordinadores/views.py
+++ coordinadores/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Coordinador
+from .forms import CoordinadorForm
 
 # Create your views here.
+def actualizar_coordinador(request, id):
+    coordinador = get_object_or_404(Coordinador, id=id)
+
+    if request.method == 'POST':
+        formulario = CoordinadorForm(request.POST, instance=coordinador)
+
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponse("<h1>Coordinador modificado con exito</h1>")
+
+    else:
+        formulario = CoordinadorForm(instance=coordinador)
+
+    context = {'formulario': formulario}
+
+    return render(request, 'modificar_coordinador.jinja', context)
