db08aa7013442ae62a0c071e8f19cb48af09acb9
Mati <matigomez2173@gmail.com>
2023-05-23 15:18:39 -0300

    Para activar un cliente

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Cliente
 
 # Create your views here.
+def activar_cliente(request, id):
+    cliente = Cliente.objects.get(id=id)
+    cliente.activo = True
+    cliente.save()
+    return HttpResponse("<h1>Cliente activado con exito</h1>")
