992a76569798940407aa1d13f6c32d249d3eca2a
arielfherrera <arielfherrera@gmail.com>
2023-05-24 16:12:05 -0300

    form, view para agregar cliente

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('agregar/', views.agregar_cliente, name='agregar-empleado'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import ClienteForm
 
 # Create your views here.
+
+def agregar_cliente(request):
+    formulario = ClienteForm()
+    if request.method == 'POST':
+        formulario = ClienteForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+        else:
+            return HttpResponseRedirect('/agregar_cliente/')
+    contexto = {
+        'formulario' : formulario
+    }
+    return render(request, 'agregar_cliente.jinja', context=contexto)
