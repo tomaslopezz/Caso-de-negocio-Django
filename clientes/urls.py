7dfee7bab6f2b19905b18319da5c9753675e7973
arielfherrera <arielfherrera@gmail.com>
2023-05-24 17:58:03 -0300

    view para listar clientes

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('listar/', views.listar_clientes, name='listar-clientes'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Cliente
 
 # Create your views here.
+def listar_clientes(request):
+    clientes = Cliente.objects.all()
+    context = {'clientes': clientes}
+
+    return render(request, 'lista_clientes.jinja', context)
