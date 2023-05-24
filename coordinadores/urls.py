4e801a1984331093848e691a61d7fca989a276d7
Tomás <98302023+tomaslopezz@users.noreply.github.com>
2023-05-24 16:35:14 -0300

    Feature forms cliente (#23)

--- coordinadores/urls.py
+++ coordinadores/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('listado', views.listar_coordinadores, name='listar-coordinadores'),
+    path('modificar/<int:id>', views.actualizar_coordinador, name='modificar-coordinador'),
+    path('nuevo', views.registrar_coordinador, name='registrar-coordinador'),
+    path('desactivar/<int:id>', views.desactivar_coordinador, name='desactivar-coordinador'),
 ]
+    
