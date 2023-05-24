68d3a7e0c1ca57cf1a729194c18c79dfb56245c9
Matias Gomez <115599228+Mati2173@users.noreply.github.com>
2023-05-24 16:45:14 -0300

    Se agrego la view para actualizar un cliente (#26)

--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.http import HttpResponseRedirect
-from .forms import ClienteForm
 from .models import Cliente
+from .forms  import ClienteForm 
 
 # Create your views here.
 
@@ -24,3 +24,21 @@ def activar_cliente(request, id):
     cliente.activo = True
     cliente.save()
     return HttpResponse("<h1>Cliente activado con exito</h1>")
+
+
+def actualizar_cliente(request, id):
+    cliente = get_object_or_404(Cliente, id=id)
+
+    if request.method == 'POST':
+        formulario = ClienteForm(request.POST, instance=cliente)
+
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponse("<h1>Cliente modificado con exito</h1>")
+        
+        else:
+            formulario = ClienteForm(instance=cliente)
+        
+        context = {'formulario':formulario}
+
+        return render(request, 'modificar_cliente.jinja', context)
