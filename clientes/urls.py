c1815e12caca7ce1f6e4cfc1970f87143cf1311e
arielfherrera <arielfherrera@gmail.com>
2023-05-27 17:15:32 -0300

    Correcciones en clientes y coordinadores

--- clientes/urls.py
+++ clientes/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('listar/', views.listar_clientes, name='listar-clientes'),
     path('modificar/<int:id>', views.modificar_cliente, name='modificar-cliente'),
     path('agregar/', views.agregar_cliente, name='agregar-cliente'),
-    path('activar/<int:id>', views.activar_cliente, name='activar-cliente')
+    path('activar/<int:id>', views.activar_cliente, name='activar-cliente'),
+    path('desactivar/<int:id>', views.desactivar_cliente, name='desactivar-cliente')
 ]
 
