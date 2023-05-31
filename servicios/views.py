e60ed5faed6b545e72d979a9785a6b94989f18d6
Mati <matigomez2173@gmail.com>
2023-05-30 21:43:42 -0300

    Para activar el registro de un servicio

--- servicios/views.py
+++ servicios/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, HttpResponse
+from .models import Servicio
+
 
 # Create your views here.
+
+def activar_servicio(request, id):
+    try:
+        servicio = Servicio.objects.get(id=id)
+        servicio.activo = True
+        servicio.save()
+        return HttpResponse("<h1>Servicio activado con exito</h1>")
+    except ObjectDoesNotExist as e:
+        return HttpResponse("<h1>Servicio inexistente</h1>")
