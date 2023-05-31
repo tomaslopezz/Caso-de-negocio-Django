d699b834bd1290cbd059c8c75572edfbb56aa470
Mati <matigomez2173@gmail.com>
2023-05-31 15:06:19 -0300

    View y url para eliminar reserva

--- reservas/views.py
+++ reservas/views.py
@@ -1,3 +1,14 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Reserva
+
 
 # Create your views here.
+def eliminar_reserva(request, id):
+    try:
+        reserva = Reserva.objects.get(id=id)
+        reserva.delete()
+        return HttpResponse("<h1>Reserva eliminada con exito</h1>")
+    except ObjectDoesNotExist as e:
+        return HttpResponse("<h1>Reserva inexistente</h1>")
