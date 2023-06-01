738e4738f7daf6a9dd9e6b36cf299ac23fcec772
Matias Gomez <115599228+Mati2173@users.noreply.github.com>
2023-06-01 12:56:48 -0300

    Tickets de Servicios y Reservas (#43)
    Co-authored-by: arielfherrera <arielfherrera@gmail.com>

--- servicios/views.py
+++ servicios/views.py
@@ -1,10 +1,24 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseRedirect
 from .models import Servicio
 from .forms import ServicioForm
 
 
 # Create your views here.
+def agregar_servicio(request):
+    formulario = ServicioForm()
+    if request.method == 'POST':
+        formulario = ServicioForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+        else:
+            return HttpResponseRedirect('/agregar_servicio/')
+    contexto = {
+        'formulario': formulario
+    }
+    return render(request, 'agregar_servicio.html', context=contexto)
+
 
 def activar_servicio(request, id):
     try:
@@ -30,9 +44,9 @@ def modificar_servicio(request, id):
         else:
             formulario = ServicioForm(instance=servicio)
 
-        contexto = {'formulario': formulario}
+        context = {'formulario': formulario}
 
-        return render(request, 'modificar_servicio.html', contexto)
+        return render(request, 'modificar_servicio.html', context)
 
     except ObjectDoesNotExist as e:
         return HttpResponse("<h1>Servicio inexistente</h1>")
