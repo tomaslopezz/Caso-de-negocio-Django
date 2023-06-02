37a923683cc60b7c2a99bee939f2406882f698b1
Mati <matigomez2173@gmail.com>
2023-06-02 00:46:25 -0300

    Corregido el modelo

--- reservas/forms.py
+++ reservas/forms.py
@@ -6,9 +6,9 @@ class ReservaForm(forms.ModelForm):
     class Meta:
         model = Reserva
         fields = ('fecha_reserva', 'fecha_servicio', 'cliente',
-                   'servicio', 'empleado', 'coordinador')
-        labels = {'fecha_reserva': 'Fecha de la Reserva',
-                  'fecha_servicio': 'Fecha del Servicio',
+                  'servicio', 'empleado', 'coordinador')
+        labels = {'fecha_reserva': 'Fecha de la reserva',
+                  'fecha_servicio': 'Fecha del servicio',
                   'cliente': 'Cliente',
                   'servicio': 'Servicio',
                   'empleado': 'Empleado',
--- reservas/models.py
+++ reservas/models.py
@@ -4,12 +4,12 @@ from servicios.models import Servicio
 from empleados.models import Empleado
 from coordinadores.models import Coordinador
 
+
 # Create your models here.
 class Reserva(models.Model):
     fecha_reserva = models.DateField()
     fecha_servicio = models.DateField()
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, limit_choices_to={'activo':True})
-    servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE, limit_choices_to={'activo':True})
-    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE, limit_choices_to={'activo':True})
-    coordinador = models.ForeignKey(Coordinador, on_delete=models.CASCADE, limit_choices_to={'activo':True})
-    activo = models.BooleanField(default=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, limit_choices_to={'activo': True})
+    servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE, limit_choices_to={'activo': True})
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE, limit_choices_to={'activo': True})
+    coordinador = models.ForeignKey(Coordinador, on_delete=models.CASCADE, limit_choices_to={'activo': True})
