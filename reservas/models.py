2f49d45cac61631748d79869e0eb811180d6777e
Mati <matigomez2173@gmail.com>
2023-06-02 14:01:52 -0300

    Nuevas correcciones en el modelo

--- reservas/models.py
+++ reservas/models.py
@@ -7,7 +7,7 @@ from coordinadores.models import Coordinador
 
 # Create your models here.
 class Reserva(models.Model):
-    fecha_reserva = models.DateField()
+    fecha_reserva = models.DateField(auto_now_add=True)
     fecha_servicio = models.DateField()
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, limit_choices_to={'activo': True})
     servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE, limit_choices_to={'activo': True})
