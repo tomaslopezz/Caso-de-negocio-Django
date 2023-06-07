22c4721ba3e1676a338b962e7cad76c0b0a66e4a
Mati <matigomez2173@gmail.com>
2023-06-07 16:12:30 -0300

    home view y url

--- Eventos_Salta/urls.py
+++ Eventos_Salta/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 
 urlpatterns = [
+    path('', views.home_page),
     path('admin/', admin.site.urls),
     path('coordinadores/', include('coordinadores.urls')),
     path('reservas/', include('reservas.urls')),
