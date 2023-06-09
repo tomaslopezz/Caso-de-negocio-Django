877587472c42bc42edab5c28916910423a45713c
Mati <matigomez2173@gmail.com>
2023-06-09 13:45:15 -0300

    Documentacion: Docstring

--- reservas/views.py
+++ reservas/views.py
@@ -7,6 +7,22 @@ from .models import Reserva, reserva_modificada, reserva_inexistente
 
 # Create your views here.
 def agregar_reserva(request):
+    """
+    Vista para agregar un reserva.
+
+    Permite al usuario agregar un reserva utilizando el formulario ReservaForm.
+
+    Método HTTP admitidos: GET, POST.
+
+    Si el método es POST y el formulario es válido, el reserva se guarda en la base de datos y se redirige a la
+    página de avisos.
+    Si el método es POST y el formulario no es válido, se redirige a la página de agregar reserva para corregir
+    los errores.
+
+    Parámetro request: Objeto HttpRequest que contiene los datos de la solicitud.
+    return: HttpResponse con la representación de la plantilla 'agregar_reserva.html' que muestra un formulario para
+    agregar un reserva.
+    """
     if request.method == 'POST':
         formulario = ReservaForm(request.POST)
         if formulario.is_valid():
@@ -24,12 +40,36 @@ def agregar_reserva(request):
 
 
 def listar_reservas(request):
+    """
+    Vista para listar todos los reservas.
+
+    Recupera todos los objetos Reserva de la base de datos y los pasa como contexto a la
+    plantilla 'listar_reservas.html'.
+
+    Parámetro request: Objeto HttpRequest que contiene los datos de la solicitud.
+    return: HttpResponse con la representación de la plantilla 'listar_reservas.html' que muestra la lista de reservas.
+    """
     reservas = Reserva.objects.all()
     contexto = {'reservas': reservas}
     return render(request, 'listar_reservas.html', contexto)
 
 
 def modificar_reserva(request, id):
+    """
+    Vista para modificar un reserva.
+
+    Recibe el ID del reserva como parámetro y busca un objeto Reserva correspondiente en la base de datos.
+    Si encuentra el reserva, se crea una instancia de ReservaForm con los datos del reserva.
+    Si el método de la solicitud es POST, se valida el formulario y se guarda la modificación en la base de datos.
+    Retorna una HttpResponse con un mensaje de éxito si el reserva se modificó correctamente.
+    Si el método de la solicitud es GET, se muestra el formulario con los datos actuales del reserva.
+    Retorna una HttpResponse con un mensaje de error si el reserva no existe.
+
+    Parámetro request: Objeto HttpRequest que contiene los datos de la solicitud.
+    Parámetro id: ID del reserva a modificar.
+    return: HttpResponse con un mensaje de éxito o error, o una representación de la plantilla 'modificar_reserva.html'
+    con el formulario y el contexto.
+    """
     try:
         reserva = Reserva.objects.get(id=id)
 
@@ -54,6 +94,18 @@ def modificar_reserva(request, id):
 
 
 def eliminar_reserva(request, id):
+    """
+    Vista para eliminar un reserva.
+
+    Recibe el ID del reserva como parámetro y busca un objeto Reserva correspondiente en la base de datos.
+    Si encuentra el reserva, la elimina y guarda los cambios en la base de datos.
+    Retorna una HttpResponse con un mensaje de éxito si el reserva se eliminó correctamente.
+    Retorna una HttpResponse con un mensaje de error si el reserva no existe.
+
+    Parámetro request: Objeto HttpRequest que contiene los datos de la solicitud.
+    Parámetro id: ID del reserva a eliminar.
+    return: HttpResponse con un mensaje de éxito o error y la opción de regresar al listado.
+    """
     contexto = dict()
     try:
         reserva = Reserva.objects.get(id=id)
--- servicios/views.py
+++ servicios/views.py
@@ -7,6 +7,22 @@ from .forms import ServicioForm
 
 # Create your views here.
 def agregar_servicio(request):
+    """
+    Vista para agregar un servicio.
+
+    Permite al usuario agregar un servicio utilizando el formulario ServicioForm.
+
+    Método HTTP admitidos: GET, POST.
+
+    Si el método es POST y el formulario es válido, el servicio se guarda en la base de datos y se redirige a la
+    página de avisos.
+    Si el método es POST y el formulario no es válido, se redirige a la página de agregar servicio para corregir
+    los errores.
+
+    Parámetro request: Objeto HttpRequest que contiene los datos de la solicitud.
+    return: HttpResponse con la representación de la plantilla 'agregar_servicio.html' que muestra un formulario para
+    agregar un servicio
+    """
     formulario = ServicioForm()
     if request.method == 'POST':
         formulario = ServicioForm(request.POST)
@@ -21,12 +37,33 @@ def agregar_servicio(request):
 
 
 def listar_servicios(request):
+    """
+    Vista para listar todos los servicios.
+
+    Recupera todos los objetos Servicio de la base de datos y los pasa como contexto a la
+    plantilla 'listar_servicios.html'.
+
+    Parámetro request: Objeto HttpRequest que contiene los datos de la solicitud.
+    return: HttpResponse con la representación de la plantilla 'listar_servicios.html' que muestra la lista de servicios
+    """
     servicios = Servicio.objects.all()
     contexto = {'servicios': servicios}
     return render(request, 'listar_servicios.html', contexto)
 
 
 def activar_servicio(request, id):
+    """
+    Vista para activar un servicio.
+
+    Recibe el ID del servicio como parámetro y busca un objeto Servicio correspondiente en la base de datos.
+    Si encuentra el servicio, establece su atributo 'activo' como True y guarda los cambios en la base de datos.
+    Retorna una HttpResponse con un mensaje de éxito si el servicio se activó correctamente.
+    Retorna una HttpResponse con un mensaje de error si el servicio no existe.
+
+    Parámetro request: Objeto HttpRequest que contiene los datos de la solicitud.
+    Parámetro id: ID del servicio a activar.
+    return: HttpResponse con un mensaje de éxito o error y la opción de regresar al listado.
+    """
     contexto = dict()
     try:
         servicio = Servicio.objects.get(id=id)
@@ -43,6 +80,18 @@ def activar_servicio(request, id):
 
 
 def desactivar_servicio(request, id):
+    """
+    Vista para desactivar un servicio.
+
+    Recibe el ID del servicio como parámetro y busca un objeto Servicio correspondiente en la base de datos.
+    Si encuentra el servicio, establece su atributo 'activo' como False y guarda los cambios en la base de datos.
+    Retorna una HttpResponse con un mensaje de éxito si el servicio se desactivó correctamente.
+    Retorna una HttpResponse con un mensaje de error si el servicio no existe.
+
+    Parámetro request: Objeto HttpRequest que contiene los datos de la solicitud.
+    Parámetro id: ID del servicio a desactivar.
+    return: HttpResponse con un mensaje de éxito o error y la opción de regresar al listado.
+    """
     contexto = dict()
     try:
         servicio = Servicio.objects.get(id=id)
@@ -59,6 +108,22 @@ def desactivar_servicio(request, id):
 
 
 def modificar_servicio(request, id):
+    """
+    Vista para modificar un servicio.
+
+    Recibe el ID del servicio como parámetro y busca un objeto Servicio correspondiente en la base de datos.
+
+    Si encuentra el servicio, se crea una instancia de ServicioForm con los datos del servicio.
+    Si el método de la solicitud es POST, se valida el formulario y se guarda la modificación en la base de datos.
+    Retorna una HttpResponse con un mensaje de éxito si el servicio se modificó correctamente.
+    Si el método de la solicitud es GET, se muestra el formulario con los datos actuales del servicio.
+    Retorna una HttpResponse con un mensaje de error si el servicio no existe.
+
+    Parámetro request: Objeto HttpRequest que contiene los datos de la solicitud.
+    Parámetro id: ID del servicio a modificar.
+    return: HttpResponse con un mensaje de éxito o error, o una representación de la plantilla 'modificar_servicio.html'
+    con el formulario y el contexto.
+    """
     try:
         servicio = Servicio.objects.get(id=id)
 
